1bd36ab8b49aeb37981fefcb174122aa2030873f
ayaanpuri30 <aypuri@ucdavis.edu>
2024-02-03 15:49:44 -0800

    frontend and backend setup

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+import IndexMSG from "./_components/indexMSG"
+export default function Home (){
+    return (
+        <div>
+            <h1>Hey</h1>
+            <IndexMSG></IndexMSG>
+        </div>
+    )
+}
